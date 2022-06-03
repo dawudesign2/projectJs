96e462bb475cfc6761495b635fabc8a996bc18ac
Dawudesign <contact@dawudesign.fr>
2022-06-03 20:59:27 +0200

    design

--- src/components/header.js
+++ src/components/header.js
@@ -7,6 +7,9 @@ const header =  () => {
     elm.createElement('a', 'nav', [{className:"nav-link", href:"#", textContent:"Home"}]);
     elm.createElement('a', 'nav', [{className:"nav-link", href:"#", textContent:"About"}]);
     elm.createElement('a', 'nav', [{className:"nav-link", href:"#", textContent:"Contact"}]);
+
+    elm.createElement("div","header",[{className:"logo"}]);
+    elm.createElement("img", "div", [{className:"logo-img", src:"src/img/D2.png" , alt:"logo"}]);
 }
 
 export default header;
